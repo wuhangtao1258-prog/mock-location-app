1724ebb5ce0d03012ee844b1bf61a2dd6acd5412
wuhangtao1258-prog <wuhangtao1258@gmail.com>
2026-09-10 12:43:12 +0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        // 百度地图官方 Maven 仓库
+        maven { url = java.net.URI("https://api.bmap.baidu.com/sdk/maven") }
     }
 }
 rootProject.name = "MockLocationApp"
